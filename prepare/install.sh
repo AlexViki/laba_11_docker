aeed38b3005a6dfc8d9dd7201a546e61aaa5bc95
Alex <shevashokc@gmail.com>
2020-08-10 20:11:15 +0300

    Update install.sh

--- prepare/install.sh
+++ prepare/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get upgrade
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get update -y
+sudo apt-get upgrade -y
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt update
-sudo apt install docker-ce
+sudo apt update -y
+sudo apt install docker-ce -y
